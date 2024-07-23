6f67c2592fc0511b6af3f1999d29a2f032e4f4fc
Tweedle2Dum <utkarsh.work1405@gmail.com>
2024-07-24 01:32:50 +0530

    refactor code, fix exiting raw mode on exit when pressing ctrl q

--- cmd/main.go
+++ cmd/main.go
@@ -1,39 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"os"
-	"syscall"
-	"unicode"
+import "github.com/godit/internal/editor"
 
-	"github.com/godit/internal/editor"
-)
-
-func CTRLKEY (c rune) rune {
-	return c & 0x1f
-} 
 
 func main() {
-	t:= editor.EnableRawMode()
-	defer editor.DisableRawMode(t)
-	for {
-		var buffer [1]byte
-		buffer[0] = '\000'
-		_, err := os.Stdin.Read(buffer[:])
-		if err != nil && err!=io.EOF && err != syscall.EAGAIN  {
-			editor.DisableRawMode(t)
-			editor.Die("read",err)
-		}
-		c:= rune(buffer[0])
-		
-		if unicode.IsControl(c) {
-			fmt.Printf("%d\r\n", c)
-
-		}else{
-			fmt.Printf("%d ('%c')\r\n",c,c)
-		}
-		if(c == CTRLKEY('q')) {break}
+	 editor.EnableRawMode()
+	for{
+		editor.RefreshScreen()
+		editor.ProcessKeyPress()
 	}
 
 }
--- internal/editor/editor.go
+++ internal/editor/editor.go
@@ -2,9 +2,19 @@ package editor
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"syscall"
+
 	"golang.org/x/sys/unix"
 )
+
+var originaltermios *unix.Termios
+
+func CTRLKEY (c rune) rune {
+	return c & 0x1f
+} 
+
 
 
 func Die (s string, err error)  {
@@ -16,13 +26,14 @@ func Die (s string, err error)  {
 	os.Exit(1)
 }
 
-func EnableRawMode() (*unix.Termios) {
+func EnableRawMode()  {
 	fd := int(os.Stdin.Fd())
-	t, err := unix.IoctlGetTermios(fd, unix.TCGETS)
+	var err error
+	originaltermios, err = unix.IoctlGetTermios(fd, unix.TCGETS)
 	if err != nil {
 		Die("TCGETS",err)
 	}
-	ct := *t
+	ct := *originaltermios
 
 	ct.Lflag = ct.Lflag &^ (unix.ECHO | unix.ICANON | unix.ISIG | unix.IEXTEN )
 	ct.Iflag = ct.Iflag &^(unix.IXON | unix.ICRNL | unix.BRKINT | unix.INPCK | unix.ISTRIP)
@@ -35,15 +46,45 @@ func EnableRawMode() (*unix.Termios) {
 		Die("TCSETS",err)
 	}
 
-	return t
+	
 
 }
 
-func DisableRawMode(s *unix.Termios) error {
+func disableRawMode() {
 	fd := int(os.Stdin.Fd())
-	if err := unix.IoctlSetTermios(fd, unix.TCSETS, s); err != nil {
-		return fmt.Errorf("error restoring terminal state: %v", err)
+	if err := unix.IoctlSetTermios(fd, unix.TCSETS, originaltermios); err != nil {
+		Die("error restoring terminal state",err)
 	}
 
-	return nil
+	
+}
+
+func editorReadKey () rune {
+	var buffer [1]byte
+		buffer[0] = '\000'
+		_, err := os.Stdin.Read(buffer[:])
+		if err != nil && err!=io.EOF && err != syscall.EAGAIN  {
+			Die("read",err)
+		}
+		return rune(buffer[0])
+}
+
+
+func ProcessKeyPress () {
+	c:= editorReadKey()
+	switch c {
+	case CTRLKEY('q') :
+		os.Stdout.Write([]byte("\x1b[2J"))
+		os.Stdout.Write([]byte("\x1b[H"))
+		disableRawMode()
+		os.Exit(0)
+	}
+}
+
+
+
+
+func RefreshScreen () {
+	os.Stdout.Write([]byte("\x1b[2J"))
+	os.Stdout.Write([]byte("\x1b[H"))
 }
