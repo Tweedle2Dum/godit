5faea89162ef7f95c166cd0d5b3660af63d52bfc
Tweedle2Dum <utkarsh.work1405@gmail.com>
2024-07-24 00:13:13 +0530

    feat. functional raw mode

--- cmd/main.go
+++ cmd/main.go
@@ -2,26 +2,36 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"syscall"
+	"unicode"
+
 	"github.com/godit/internal/editor"
 )
 
 func main() {
-	var buffer [1]byte
-	t,err := editor.EnableRawMode()
-	if(err!=nil){
-		return
-	}
+	t:= editor.EnableRawMode()
 	defer editor.DisableRawMode(t)
 	for {
+		var buffer [1]byte
+		buffer[0] = '\000'
 		n, err := os.Stdin.Read(buffer[:])
-		if err != nil {
-			fmt.Println("Error reading input:", err)
+		if err != nil && err!=io.EOF && err != syscall.EAGAIN  {
+			editor.DisableRawMode(t)
+			editor.Die("read",err)
 		}
 		if n>0 && buffer[0] == 'q' {
 			fmt.Println("\nExiting...")
 			break
 		}
+		if unicode.IsControl(rune(buffer[0])) {
+			fmt.Printf("%d\r\n", rune(buffer[0]))
+
+		}else{
+			fmt.Printf("%d ('%c')\r\n",rune(buffer[0]),rune(buffer[0]))
+		}
+		
 	}
 
 }
--- internal/editor/editor.go
+++ internal/editor/editor.go
@@ -3,27 +3,39 @@ package editor
 import (
 	"fmt"
 	"os"
-
 	"golang.org/x/sys/unix"
 )
 
-func EnableRawMode() (*unix.Termios, error) {
+
+func Die (s string, err error)  {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", s, err)
+	} else {
+		fmt.Fprintf(os.Stderr, "%s\n", s)
+	}
+	os.Exit(1)
+}
+
+func EnableRawMode() (*unix.Termios) {
 	fd := int(os.Stdin.Fd())
 	t, err := unix.IoctlGetTermios(fd, unix.TCGETS)
 	if err != nil {
-		fmt.Println("Error getting terminal state for raw mode", err)
-		return nil, err
+		Die("TCGETS",err)
 	}
 	ct := *t
 
-	ct.Lflag = ct.Lflag &^ (unix.ECHO | unix.ICANON)
-
+	ct.Lflag = ct.Lflag &^ (unix.ECHO | unix.ICANON | unix.ISIG | unix.IEXTEN )
+	ct.Iflag = ct.Iflag &^(unix.IXON | unix.ICRNL | unix.BRKINT | unix.INPCK | unix.ISTRIP)
+	ct.Oflag = ct.Oflag &^(unix.OPOST)
+	ct.Cflag = ct.Cflag &^(unix.CS8)
+	ct.Cc[unix.VMIN] = 0 
+	ct.Cc[unix.VTIME] = 1
 	if err := unix.IoctlSetTermios(fd, unix.TCSETS, &ct); err != nil {
 		fmt.Println("Error setting terminal to raw mode:", err)
-		return nil, err
+		Die("TCSETS",err)
 	}
 
-	return t, nil
+	return t
 
 }
 
