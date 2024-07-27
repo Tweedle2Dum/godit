f0d2604d95925ea98cc7d107092d5c2c93a55626
Tweedle2Dum <utkarsh.work1405@gmail.com>
2024-07-27 12:25:50 +0530

    fix. infinite tilde draw

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import "github.com/godit/internal/editor"
 
 func main() {
 	 editor.EnableRawMode()
+	 editor.InitEditor()
+
 	for{
 		editor.RefreshScreen()
 		editor.ProcessKeyPress()
--- internal/editor/editor.go
+++ internal/editor/editor.go
@@ -1,27 +1,43 @@
 package editor
 
 import (
+	"fmt"
+	"golang.org/x/sys/unix"
 	"io"
 	"os"
 	"syscall"
-	"golang.org/x/sys/unix"
 )
 
-var originaltermios *unix.Termios
+type Editor struct {
+	t     *unix.Termios
+	srows int
+	scol  int
+}
 
-func CTRLKEY (c rune) rune {
+var e Editor
+
+func CTRLKEY(c rune) rune {
 	return c & 0x1f
-} 
+}
 
-
-func safeExit(){
+func safeExit() {
 	disableRawMode()
 	os.Exit(0)
 }
 
+func editorDrawRows() {
+	var r int
+	for r = 0; r < e.srows; r++ {
+		os.Stdout.Write([]byte("~"))
+		if(r < e.srows-1) {
+			os.Stdout.Write([]byte("\r\n"))
 
-func Die (s string, err error)  {
-	
+		}
+	}
+}
+
+func Die(s string, err error) {
+
 	os.Stderr.Write([]byte(s))
 	os.Stderr.Write([]byte("\t -------> "))
 	os.Stderr.Write([]byte(err.Error()))
@@ -29,26 +45,24 @@ func Die (s string, err error)  {
 	os.Exit(1)
 }
 
-func EnableRawMode()  {
+func EnableRawMode() {
 	fd := int(os.Stdin.Fd())
 	var err error
-	originaltermios, err = unix.IoctlGetTermios(fd, unix.TCGETS)
+	e.t, err = unix.IoctlGetTermios(fd, unix.TCGETS)
 	if err != nil {
-		Die("TCGETS",err)
+		Die("TCGETS", err)
 	}
-	ct := *originaltermios
+	ct := *e.t
 
-	ct.Lflag = ct.Lflag &^ (unix.ECHO | unix.ICANON | unix.ISIG | unix.IEXTEN )
-	ct.Iflag = ct.Iflag &^(unix.IXON | unix.ICRNL | unix.BRKINT | unix.INPCK | unix.ISTRIP)
-	ct.Oflag = ct.Oflag &^(unix.OPOST)
-	ct.Cflag = ct.Cflag &^(unix.CS8)
-	ct.Cc[unix.VMIN] = 0 
+	ct.Lflag = ct.Lflag &^ (unix.ECHO | unix.ICANON | unix.ISIG | unix.IEXTEN)
+	ct.Iflag = ct.Iflag &^ (unix.IXON | unix.ICRNL | unix.BRKINT | unix.INPCK | unix.ISTRIP)
+	ct.Oflag = ct.Oflag &^ (unix.OPOST)
+	ct.Cflag = ct.Cflag &^ (unix.CS8)
+	ct.Cc[unix.VMIN] = 0
 	ct.Cc[unix.VTIME] = 1
 	if err := unix.IoctlSetTermios(fd, unix.TCSETS, &ct); err != nil {
-		Die("TCSETS",err)
+		Die("TCSETS", err)
 	}
-
-	
 
 }
 
@@ -56,36 +70,71 @@ func disableRawMode() {
 	fd := int(os.Stdin.Fd())
 	os.Stdout.Write([]byte("\x1b[2J"))
 	os.Stdout.Write([]byte("\x1b[H"))
-	if err := unix.IoctlSetTermios(fd, unix.TCSETS, originaltermios); err != nil {
-		Die("error restoring terminal state",err)
+	if err := unix.IoctlSetTermios(fd, unix.TCSETS, e.t); err != nil {
+		Die("error restoring terminal state", err)
 	}
 }
 
-func editorReadKey () rune {
+func editorReadKey() rune {
 	var buffer [1]byte
-		buffer[0] = '\000'
-		_, err := os.Stdin.Read(buffer[:])
-		if err != nil && err!=io.EOF && err != syscall.EAGAIN  {
-			Die("read",err)
-		}
-		return rune(buffer[0])
+	buffer[0] = '\000'
+	_, err := os.Stdin.Read(buffer[:])
+	if err != nil && err != io.EOF && err != syscall.EAGAIN {
+		Die("read", err)
+	}
+	return rune(buffer[0])
 }
 
-
-func ProcessKeyPress () {
-	c:= editorReadKey()
+func ProcessKeyPress() {
+	c := editorReadKey()
 	switch c {
-	case CTRLKEY('q') :
+	case CTRLKEY('q'):
 		os.Stdout.Write([]byte("\x1b[2J"))
 		os.Stdout.Write([]byte("\x1b[H"))
 		safeExit()
 	}
 }
 
-
-
-
-func RefreshScreen () {
+func RefreshScreen() {
 	os.Stdout.Write([]byte("\x1b[2J"))
 	os.Stdout.Write([]byte("\x1b[H"))
+	editorDrawRows()
+	os.Stdout.Write([]byte("\x1b[H"))
+
+}
+
+func getWindowSize(rows *int, cols *int) (int, error) {
+	fd := int(os.Stdin.Fd())
+	s, err := unix.IoctlGetWinsize(fd, unix.TIOCGWINSZ)
+	if err != nil && s.Col == 0 {
+		w, err := os.Stdout.Write([]byte("\x1b[999C\x1b[999B"))
+		if w != 12 {
+			return -1, err
+		}
+		c, err := getCursorPosition(&e.srows, &e.scol)
+		return c, err
+	}
+	*rows = int(s.Row)
+	*cols = int(s.Col)
+	return 0, nil
+}
+
+func InitEditor() {
+	c, err := getWindowSize(&e.srows, &e.scol)
+	if c == -1 {
+		Die("getWindowSize", err)
+	}
+
+}
+
+func getCursorPosition(rows *int, cols *int) (int, error) {
+	if _, err := os.Stdout.Write([]byte("\x1b[6n")); err != nil {
+		return -1, err
+	}
+
+	if _, err := fmt.Fscanf(os.Stdin, "\x1b[%d;%d", rows, cols); err != nil {
+		return -1, err
+	}
+
+	return 0, nil
 }
